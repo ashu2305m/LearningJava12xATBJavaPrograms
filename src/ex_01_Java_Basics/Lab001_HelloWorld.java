a09a097a77ed8c5ee608c1733c50d61bf010b99c
Ashutosh Choubey <ashutoshchoubey16@gmail.com>
2025-05-07 21:35:46 +0530

    new changes

--- src/ex_01_Java_Basics/Lab001_HelloWorld.java
+++ src/ex_01_Java_Basics/Lab001_HelloWorld.java
@@ -5,13 +5,7 @@ public class Lab001_HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello Ashutosh");
-        //
-         //
-         //
-        //
-        //
-        //
-        //
+
 
     }
 }
