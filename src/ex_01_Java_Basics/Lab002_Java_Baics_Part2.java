a300f46bea129a6dac1fbfa5a7b80f5c42427578
Ashutosh Choubey <ashutoshchoubey16@gmail.com>
2025-05-09 19:36:34 +0530

    new changes

---
+++ src/ex_01_Java_Basics/Lab002_Java_Baics_Part2.java
@@ -0,0 +1,11 @@
+package ex_01_Java_Basics;
+
+public class Lab002_Java_Baics_Part2 {
+    public static void main(String[] args) {
+        System.out.println("Hello Ashutosh! This is part 2 of Java Basics");
+    }
+
+}
+
+
+
