96738282cc97aaf54546d29f037e6d55c655460c
Ashutosh Choubey <ashutoshchoubey16@gmail.com>
2025-05-12 23:24:16 +0530

    loops

---
+++ src/ex_04_loops/lab001_loop1.java
@@ -0,0 +1,10 @@
+package ex_04_loops;
+
+import com.sun.tools.javac.Main;
+
+public class lab001_loop1 {
+
+    public static void main(String[] args) {
+        System.out.println("This is a loop program");
+    }
+}
