7ff9dafa7fb481a76b32939b40fad1430ab9008d
Ashutosh Choubey <ashutoshchoubey16@gmail.com>
2025-05-12 23:20:23 +0530

    aaaayfth

---
+++ src/Tasks/Task1.java
@@ -0,0 +1,16 @@
+package Tasks;
+
+public class Task1 {
+    // Task #1
+    //
+    //Table of 5 you need to print,  (only use the printF)
+    //int num = 5;
+    //
+    //5×1=5
+    //5×2 = 10
+    //….
+    //5×10 =50
+
+    public static void main(String[] args) {
+    }
+}
