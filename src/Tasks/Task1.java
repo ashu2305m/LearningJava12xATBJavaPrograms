a4a14f30ce719ab321080fa3ebc6a98ea0b1b0d4
Ashutosh Choubey <ashutoshchoubey16@gmail.com>
2025-05-14 20:33:28 +0530

    task1

--- src/Tasks/Task1.java
+++ src/Tasks/Task1.java
@@ -18,27 +18,16 @@ public class Task1 {
         int num=5;
 
 
-        System.out.printf("%d*1=%d",num,num*1);
-        System.out.println();
-        System.out.printf("%d*2=%d",num,num*2);
-        System.out.println();
-        System.out.printf("%d*3=%d",num,num*3);
-        System.out.println();
-        System.out.printf("%d*4=%d",num,num*4);
-        System.out.println();
-        System.out.printf("%d*5=%d",num,num*5);
-        System.out.println();
-        System.out.printf("%d*6=%d",num,num*6);
-        System.out.println();
-        System.out.printf("%d*7=%d",num,num*7);
-        System.out.println();
-        System.out.printf("%d*8=%d",num,num*8);
-        System.out.println();
-        System.out.printf("%d*9=%d",num,num*9);
-        System.out.println();
-        System.out.printf("%d*10=%d",num,num*10);
-        System.out.println();
-
+        System.out.printf("%d*1=%d\n",num,num*1);
+        System.out.printf("%d*2=%d\n",num,num*2);
+        System.out.printf("%d*3=%d\n",num,num*3);
+        System.out.printf("%d*4=%d\n",num,num*4);
+        System.out.printf("%d*5=%d\n",num,num*5);
+        System.out.printf("%d*6=%d\n",num,num*6);
+        System.out.printf("%d*7=%d\n",num,num*7);
+        System.out.printf("%d*8=%d\n",num,num*8);
+        System.out.printf("%d*9=%d\n",num,num*9);;
+        System.out.printf("%d*10=%d\n",num,num*10);
 
     }
 }
