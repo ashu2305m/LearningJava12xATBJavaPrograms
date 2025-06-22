4957f37c9463f0c8bc04731f38f5706a236a605a
Ashutosh Choubey <ashutoshchoubey16@gmail.com>
2025-06-22 23:03:49 +0530

    21 June List,Set,Queue

---
+++ src/ex_25_Generics/Lab_Generics_List.java
@@ -0,0 +1,16 @@
+package ex_25_Generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lab_Generics_List {
+    public static void main(String[] args) {
+        List num = new ArrayList<>();
+        num.add(12);
+        num.add(34);
+        num.add(90);
+        num.add("Pramod");
+        System.out.println(num);
+
+    }
+}
---
+++ src/ex_26_Collection_Framework/List/Lab229_List.java
@@ -0,0 +1,11 @@
+package ex_26_Collection_Framework.List;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lab229_List {
+    public static void main(String[] args) {
+        List mylist = new ArrayList(5); // here we have mentioned capacity as 5 but the capacity will
+        List mylist0 = new ArrayList();             //  automatically increase if we want to increase more elements
+    }
+    }
